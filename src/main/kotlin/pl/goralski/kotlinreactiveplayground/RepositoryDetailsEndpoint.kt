e80cb82c7d657412552b72ee22edc9e7772d0376
Krzysztof Góralski <krzysztof.goralski@bitgamelabs.com>
2018-02-19 21:19:20 +0100

    companion object

--- src/main/kotlin/pl/goralski/kotlinreactiveplayground/RepositoryDetailsEndpoint.kt
+++ src/main/kotlin/pl/goralski/kotlinreactiveplayground/RepositoryDetailsEndpoint.kt
@@ -3,31 +3,37 @@ package pl.goralski.kotlinreactiveplayground
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 
 
 @RestController
+@RequestMapping("repositories")
 class RepositoryDetailsEndpoint(
         private val githubClient: GithubClient,
         private val entityRepository: ReactiveEntityGithubRepository) {
 
-    @GetMapping(value = ["/repositories/{owner}/{repositoryName}"], produces = [(MediaType.APPLICATION_JSON_VALUE)])
+    @GetMapping(value = ["/{owner}/{repositoryName}"], produces = [(MediaType.APPLICATION_JSON_VALUE)])
     fun getRepositoryInfo(
-            @PathVariable("owner") owner: String,
-            @PathVariable("repositoryName") repositoryName: String): Flux<GithubRepository> {
+            @PathVariable(OWNER) owner: String,
+            @PathVariable(REPOSITORY_NAME) repositoryName: String): Flux<GithubRepository> {
 
         return githubClient.getRepository(owner, repositoryName).flatMap { it -> entityRepository.save(it) }
     }
 
-    @GetMapping(value = ["/repositories/{owner}"], produces = [(MediaType.APPLICATION_JSON_VALUE)])
-    fun getRepositoriesByOwner(@PathVariable("owner") owner: String): Flux<GithubRepository> {
+    @GetMapping(value = ["/{owner}"], produces = [(MediaType.APPLICATION_JSON_VALUE)])
+    fun getRepositoriesByOwner(@PathVariable(OWNER) owner: String): Flux<GithubRepository> {
         return githubClient.getRepositories(owner).flatMap { it -> entityRepository.save(it) }
     }
 
-    @GetMapping(value = ["/repositories/"], produces = [(MediaType.APPLICATION_JSON_VALUE)])
+    @GetMapping(produces = [(MediaType.APPLICATION_JSON_VALUE)])
     fun getRepositories(): Flux<GithubRepository> {
         return entityRepository.findAll()
     }
 
+    companion object {
+        const val OWNER: String = "owner"
+        const val REPOSITORY_NAME: String = "repositoryName"
+    }
 }
